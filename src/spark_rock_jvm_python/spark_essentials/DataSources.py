9c9a5395f4d80f5c98663b980246c0735a3ba253
Alexander Socarras <alex@Alexanders-Mac-mini.local>
2025-03-02 13:16:58 -0500

    process JSON

--- src/spark_rock_jvm_python/spark_essentials/DataSources.py
+++ src/spark_rock_jvm_python/spark_essentials/DataSources.py
@@ -19,6 +19,8 @@ Note that dataframes are written as directories with multiple files; the number 
 import logging
 import os
 
+from py4j.protocol import Py4JJavaError
+from pyspark.sql.functions import col, date_format
 from pyspark.sql.session import SparkSession
 from pyspark.sql.types import (
     DateType,
@@ -60,13 +62,29 @@ def main():
 
     cars_json = resource_path("cars.json")
 
-    ## Reading data w/ mode=failFast
-    cars_DF = (
+    ## Reading data w/ mode
+    cars_DF_inferred_schema = (
+        spark.read.format("json")
+        # .schema(car_schema)
+        .option("inferSchema", "true")
+        .option("mode", "failFast")  # dropMalformed, permissive (default)
+        .option("path", str(cars_json))
+        .load()
+    )
+    print(f"cars_DF_inferred_schema.schema:{cars_DF_inferred_schema.schema}")
+    print(f"Year column:{cars_DF_inferred_schema.select(col('Year'))}")  # string
+
+    cars_DF_explicit_schema = (
         spark.read.format("json")
         .schema(car_schema)
         .option("mode", "failFast")  # dropMalformed, permissive (default)
         .option("path", str(cars_json))
         .load()
+    )
+    print(f"cars_DF_explicit_schema.schema: {cars_DF_explicit_schema.schema}")
+    print(f"Year column: {cars_DF_explicit_schema.select(col('Year'))}")  # date
+    print(
+        "When parsing string columns to DateType, Spark assumes the standard ISO Format (YYYY-MM-DD). This column happens to be in the ISO format YYYY-MM-DD."
     )
 
     ## Using an option map
@@ -88,9 +106,62 @@ def main():
     cars_DF_dupe.show(2)
     # Note that when writing dataframes with spark, a folder is created with multiple files
     print(
-        f"Written dataframe {os.path.basename(out_path)} is a directory: {out_path.is_dir()}"
+        f"Written dataframe {os.path.basename(out_path)} is a directory: {out_path.is_dir()}\n"
     )
     print_write_path_contents(out_path, indent=4)
+
+    ## Writing DataFrames with JSON Flags
+    # Date Parsing
+    print(
+        "\nIf we had a different date format than yyyy-MM-dd, Spark would need a date format field"
+    )
+    out_path = write_path("cars_dupe_date_change.json")
+    # fmt: off
+    cars_DF_date_changed = cars_DF \
+        .withColumn("Year", date_format(col("Year"),format='dd-MM-yyyy')) \
+        .limit(5)
+    cars_DF_date_changed.write \
+        .format('json') \
+        .mode('overwrite') \
+        .option('path', str(out_path)) \
+        .save()
+
+    cars_DF_date_changed_load = (
+        spark.read
+        .format('json')
+        # .option('inferSchema', 'true') \ <- this would not fail, but would read as string
+        .schema(car_schema)
+        .option('path',str(out_path))
+        .option('mode', 'failFast')
+        .load()
+    )
+
+    def wrong_date_format_should_fail():
+        raise AssertionError('Uhhh?')
+    try:
+        cars_DF_date_changed_load.show()
+        wrong_date_format_should_fail()
+    except Py4JJavaError as e:
+        print(f'Date Parsing Error: "{str(e)[:min(len(str(e)),400)]}..."')
+
+    option_map = {
+        'path':str(out_path),
+        'mode':'failFast',
+        'dateFormat':'dd-MM-yyyy', # couple with schema
+        ## Specific JSON options
+        'allowSingleQuotes': "true",
+        'compression': 'uncompressed' # bzip2, gzip, lz4, snappy, deflate, default: uncompressed
+    }
+    cars_DF_date_changed_load = (
+        spark.read
+        .format('json')
+        .schema(car_schema)
+        .options(**option_map)
+        .load()
+    )
+    cars_DF_date_changed_load.show()
+
+    ## You can replace .format('json') and .load() with a single '.json()' call at the end
 
 
 if __name__ == "__main__":
