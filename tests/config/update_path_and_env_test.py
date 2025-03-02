a3d9d4f208e2a50897c8d3910c3fed292d3b4d92
Alexander Socarras <alexsocarras@Alexanders-MacBook-Pro.local>
2025-03-01 23:28:03 -0500

    reorganized tests

---
+++ tests/config/update_path_and_env_test.py
@@ -0,0 +1,41 @@
+import logging
+import os
+import sys
+from pathlib import Path
+
+from pyspark.sql import SparkSession
+
+from spark_rock_jvm_python.config import Config, update_path_and_env
+
+logger = logging.getLogger(__name__)
+
+
+def test_update_path_and_env(default_config: Config) -> None:
+    """Test that the env and python path are changed as intended"""
+
+    ## Check that env vars are set
+    update_path_and_env(default_config)
+
+    assert os.environ.get("JAVA_HOME") == default_config.JAVA_HOME
+    assert os.environ.get("SPARK_HOME") == default_config.SPARK_HOME
+    assert os.environ.get("PYSPARK_PYTHON") == default_config.PYSPARK_PYTHON
+
+    ## Check that values were added to the pythonpath
+    spark_python_path = Path(default_config.SPARK_HOME) / "python"
+    py4j_path = next((spark_python_path / "lib").glob("py4j*"), None)
+
+    logger.debug(sys.path.index(str(spark_python_path)))
+    logger.debug(sys.path.index(str(py4j_path)))
+
+
+def test_run_spark_session(default_config: Config) -> None:
+    """Test that these changes actually enable running pyspark"""
+    update_path_and_env(default_config)
+
+    ## Check that a spark session can be started based on these changes
+    _: SparkSession = (
+        SparkSession.Builder()
+        .appName("update_path_and_env_test")
+        .config("spark.master", "local")
+        .getOrCreate()
+    )
---
+++ tests/conftest.py
@@ -0,0 +1,70 @@
+import json
+import os
+from dataclasses import asdict, fields
+from pathlib import Path
+
+import pytest
+
+from spark_rock_jvm_python.config import (
+    Config,
+)
+
+
+@pytest.fixture
+def project_root() -> Path:
+    cur_file = Path(__file__)
+    project_root = cur_file.parents[1]
+    assert os.path.basename(project_root) == "spark-rock-jvm-python"
+    return project_root
+
+
+@pytest.fixture
+def default_config() -> Config:
+    return Config(**{f.name: f.metadata.get("example", "") for f in fields(Config)})
+
+
+@pytest.fixture
+def madeup_config() -> Config:
+    return Config(
+        JAVA_HOME="foo_openjdk@7",
+        SPARK_HOME="apache-spark/bar_exe",
+        PYSPARK_PYTHON="python2_whoops",
+    )
+
+
+@pytest.fixture
+def blank_env(monkeypatch):
+    for field in fields(class_or_instance=Config):
+        if os.environ.get(field.name):
+            monkeypatch.delenv(field.name)
+    yield
+
+
+@pytest.fixture
+def env_with_madeup_vars(monkeypatch, madeup_config):
+    for f in fields(madeup_config):
+        monkeypatch.setenv(f.name, getattr(madeup_config, f.name))
+    yield
+
+
+@pytest.fixture
+def config_file_w_extra_vars(tmp_path, default_config) -> Path:
+    config_dict_w_extra = asdict(default_config)
+    config_dict_w_extra["extra_key"] = "extra_value"
+    with open(path := (tmp_path / "config_file_w_extra.yaml"), "w") as file:
+        json.dump(config_dict_w_extra, file)
+    return path
+
+
+@pytest.fixture
+def madeup_config_path(tmp_path, madeup_config) -> Path:
+    with open(path := tmp_path / "madeup_config.yaml", "w") as file:
+        json.dump(asdict(madeup_config), file)
+    return path
+
+
+@pytest.fixture
+def default_config_path(tmp_path, default_config) -> Path:
+    with open(path := tmp_path / "default_config.yaml", "w") as file:
+        json.dump(asdict(default_config), file)
+    return path
